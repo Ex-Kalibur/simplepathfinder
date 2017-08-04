edae2c323761cf7e5c050163dd112fcd739ad8be
Christopher Collins <christopher.collins@ryerson.ca>
2017-08-04 11:28:55 -0400

    Created AStarPathfinder class

---
+++ src/main/java/pathfinders/AStarPathfinder.java
@@ -0,0 +1,21 @@
+package pathfinders;
+
+import data.Layout;
+
+/**
+ * Created by Christopher on 2017-08-04.
+ */
+public class AStarPathfinder extends AbstractPathfinder{
+
+	public AStarPathfinder(Layout layout){
+		super(layout);
+	}
+
+	@Override
+	public Path findPath(){
+		Path path = new Path();
+		//TODO - Implement pathfinding logic
+
+		return path;
+	}
+}
